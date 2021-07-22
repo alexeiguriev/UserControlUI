7471187557bca9d5f501f68a95f0cca6d8ef85e1
alexeiguriev <gG4nmam85Z4aGpfZ>
2021-07-22 10:32:05 +0300

    Add document getting feature and view

---
+++ UserControlUI/Controllers/DocumentController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using UserControlUI.Helper;
+using UserControlUI.ModelsDTO;
+
+namespace UserControlUI.Controllers
+{
+    public class DocumentController : Controller
+    {
+        UserAPI _api = new UserAPI();
+        public async Task<IActionResult> Index()
+        {
+            List<DocumentDTO> users = new List<DocumentDTO>();
+            HttpClient client = _api.Initial();
+            HttpResponseMessage res = await client.GetAsync("api/Document");
+            if (res.IsSuccessStatusCode)
+            {
+                var result = res.Content.ReadAsStringAsync().Result;
+                users = JsonConvert.DeserializeObject<List<DocumentDTO>>(result);
+            }
+            return View(users);
+        }
+        ////Create document methode
+        //public async Task<IActionResult> Create()
+        //{
+        //    List<DocumentDTO> users = new List<DocumentDTO>();
+        //    HttpClient client = _api.Initial();
+        //    HttpResponseMessage res = await client.GetAsync("api/Document");
+        //    if (res.IsSuccessStatusCode)
+        //    {
+        //        var result = res.Content.ReadAsStringAsync().Result;
+        //        users = JsonConvert.DeserializeObject<List<DocumentDTO>>(result);
+        //    }
+        //    return View(users);
+        //}
+
+    }
+}
---
+++ UserControlUI/ModelsDTO/DocumentDTO - Copy.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserControlUI.ModelsDTO
+{
+    public class DocumentInputDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public int Status { get; set; }
+        public DateTime UploadedDate { get; set; }
+        public int UpdatedByUserId { get; set; }
+        IFormFile file { get; set; }
+    }
+}
